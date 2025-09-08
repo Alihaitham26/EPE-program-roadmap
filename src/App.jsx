cd19e05edc9db010d42112392ceacac2883f9ff5
alihaitham26 <alihaithamahmed26@gmail.com>
2025-09-08 09:08:58 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Semester from "./components/Semester"
 import courseRoadmap from "./data"
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       {courseRoadmap.map((semester,index)=><Semester semesterObj={semester} semesterNumber={index + 1} key={index}/>)}
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return (
+    <footer>
+        <p>Based on the <a href="https://drive.google.com/file/d/1dQieKhCEXxfNJVxLm85U28rTf9R15qfw/view?usp=sharing">2023 official course map</a> by Cairo university faculty of engineering.</p>
+        <p>Designed and created by <a href="https://my-portfolio-gold-alpha-66.vercel.app/">Ali Haitham</a>.</p>
+    </footer>
+    )
+}
